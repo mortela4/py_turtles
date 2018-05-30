31ad09ac82f71ec855e0018beeac1525cea144b1
mortela4 <morten.larsen@7sense.no>
2018-05-30 07:53:03 +0200

    Added file.

---
+++ hedda.py
@@ -0,0 +1,21 @@
+# Oppgave:
+# skriv "HEDDA" med turtle!
+# =========================
+# Eksempel-bokstav:
+#
+# 100 -   |      |
+#         |      |
+# 50  -   |------|
+#         |      |
+# 0   -   |      |
+
+import turtle
+
+# Step 2: Create a new turtle. We'll call it "hedda"
+hedda = turtle.Turtle()
+
+# Write HEDDA ...
+hedda.forward(50)
+
+# Step 4: We're done!
+turtle.done()
